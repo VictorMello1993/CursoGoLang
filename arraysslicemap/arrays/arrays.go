308b1765ca983e7778927feb3a08c1eaa5ec7594
Victor Mello <victorsmello93@gmail.com>
2018-08-05 21:12:06 -0300

    Introdução a arrays.

---
+++ arraysslicemap/arrays/arrays.go
@@ -0,0 +1,15 @@
+//Introdução a arrays
+
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	//Arrays são estruturas homogêneas (dados de mesmo tipo) e estática (fixa)
+
+	var notas [3]float64
+	fmt.Println(notas); //Imprimindo todos os elementos do array
+	
+}
