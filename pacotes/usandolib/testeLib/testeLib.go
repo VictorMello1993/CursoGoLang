e93cc4d0026633eb1cd9b5cc68e47b3b36a948be
Victor Mello <victorsmello93@gmail.com>
2018-09-23 22:42:25 -0300

    Aula de criação e instalação de dependências criadas do GitHub.

---
+++ pacotes/usandolib/testeLib/testeLib.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/VictorMello1993/testelib"
+)
+
+func main() {
+	fmt.Println(testelib.Adicao(2, 4))
+	fmt.Println(testelib.Multiplicacao(8, 5))
+	fmt.Println(testelib.Divisao(100, 5))
+	fmt.Println(testelib.Subtracao(100, 50))
+}
---
+++ pacotes/usandolib/usandolib.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/VictorMello1993/goarea"
+)
+
+func main() {
+	fmt.Println(goarea.Circ(4.0))
+}
