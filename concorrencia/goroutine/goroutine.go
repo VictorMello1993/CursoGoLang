ef6410ea39a700591d26c4477c4de3037b7969a3
Victor Santos de Mello <victorsmello93@gmail.com>
2018-09-24 20:05:28 -0300

    Conceito básico de goroutines
    Modificada a documentação para melhor esclarecimento do conceito.

---
+++ concorrencia/goroutine/goroutine.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func falar(nome, texto string, qte int) {
+	for i := 0; i < qte; i++ {
+		time.Sleep(time.Second) //Após cada 1 segundo será executada a próxima instrução
+		fmt.Printf("%s: %s (iteração %d)\n", nome, texto, i+1)
+	}
+}
+
+func main() {
+
+	// //Execução sequencial (uma função só será executada depois que a anterior termina)
+	// falar("Victor", "Olá, Pedro", 3)
+	// falar("Pedro", "Eae, qual é a boa de hoje?", 1)
+
+	//Criando uma goroutine (função que executa de maneira independente de outras funções)
+	go falar("Victor", "Ei...", 500)
+	go falar("Vanderson", "Entendi, parça!", 500)
+
+	//É importante levar em consideração que uma goroutine só será executada quando a função main() terminar
+	//a execução.
+
+	time.Sleep(time.Second * 5)
+
+}
